30885e69211edba1a2af06f7cc51e2535565882d
basementApe <71020874+basementApe@users.noreply.github.com>
2026-07-03 15:12:07 +0200

    New placeholder methods - CertManager

--- CertManager.cs
+++ CertManager.cs
@@ -5,5 +5,17 @@ namespace CertAPI
         private readonly List<Certificate> _certificates = [];
 
         public List<Certificate> GetAll() => new List<Certificate>(_certificates);      // return a copy instead of the actual list so _certificates itself remains protected
+
+        public List<Certificate> GetByNumber()
+        {
+            var list = GetAll();
+            return list;
+        }
+
+        public List<Certificate> GetByType()
+        {
+            var list = GetAll();
+            return list;
+        }
     }
 }
